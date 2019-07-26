8b07a5e930153b3c186bd7f7f7fe837345b0a498
Ryan Day <ryaneday@google.com>
2019-07-26 09:18:29 -0700

    chore: using pa6h.delimeter to update path env

--- test/credentials-helper.ts
+++ test/credentials-helper.ts
@@ -23,10 +23,7 @@ describe('can get token from credential helper.', () => {
     const helper =
         new DockerCredentialHelpers(path.join(fixtureDir, 'docker.json'));
 
-
-    console.log('--->>>ENVVVV-->>> ',process.env)
-
-    process.env.PATH += ':' + fixtureDir;
+    process.env.PATH += path.delimiter + fixtureDir;
 
     const config = helper.getDockerConfig();
 
